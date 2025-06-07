4b9857135fddb345e742de8917964a1884a72edf
Angel <munozbenavidesangel@gmail.com>
2025-06-06 21:32:28 -0500

    Script: data types in python

---
+++ example1.py
@@ -0,0 +1,21 @@
+#line comment
+
+'''
+in blokc coment
+Dev: Angel Muñoz
+'''
+
+d1 = 10
+d2 = 3,50
+d3 = "Angel M"
+d4 = 'Angel Muñoz'
+d5 = '''Hello people, we are learning python.'''
+d6 = True
+d7 = 3.4
+print (type(d1))
+print (type(d2))
+print (type(d3))
+print (type(d4))
+print (type(d5))
+print (type(d6))
+print (type(d7))
