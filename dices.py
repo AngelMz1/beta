28f6630dd30c6595e30f29a49589d6ad85b5b2ba
Angel Muñoz <munozbenavidesangel@gmail.com>
2025-06-20 20:53:34 -0500

    actualizacion funciones

--- dices.py
+++ dices.py
@@ -8,12 +8,15 @@ print dices value
 '''
 import os
 from random import randint
-
-os.system("clear")
+#####################################
+#functions
+#####################################
 print(":::Welcome to the game:::")
+os.system("clear")
 player_name = input("Please enter your name: ")
 print(f"Hello {player_name}, your dices are")
-dice1 = randint(1,6)
-dice2 = randint(1,6)
-print(f"Dice 1: {dice1} Dice 2: {dice2}")
-
+def get_dices():
+    dice1 = randint(1,6)
+    dice2 = randint(1,6)
+    print(f"Dice 1: {dice1} Dice 2: {dice2}")
+get_dices()
