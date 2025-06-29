98d83ea5b5d995e1a72a3b0db575dfd430636f30
Angel Muñoz <munozbenavidesangel@gmail.com>
2025-06-29 15:20:08 -0500

    segunda version de generador de reportes, funciones mejoradas y validacion de edad con rango de edades validas

--- workshop1.py
+++ workshop1.py
@@ -1,8 +1,10 @@
-####Repot in python###
+#### Repot in python ###
 
+#### Imports ####
 import os
 from random import randint
 
+#### Functions ####
 def get_income():
     income = randint(1000000,8000000)
     return income
@@ -14,8 +16,30 @@ def data_valid(x):
             return entry
         else:
             print("Invalid data, please try again")
+
+def numb_valid(x):
+    while True:
+        entry =input(x).strip()
+        try:
+            number = float(entry)
+            return number
+        except:
+            print("Invalid data, please try again")
+
+def age_valid(x):
+    while True:
+        entry =input(x).strip()
+        try:
+            age = int(entry)
+            if age >= 18 and age <= 100:
+                return age
+            else:
+                print("Age out of range, please try again")
+        except:
+            print("Invalid data, please try again")
+
 def save_client(a,b,c,d,e,f,g,h,i,j):
-    client = {
+    customer = {
         "Name": a,
         "Id Number": b,
         "Age": c,
@@ -27,30 +51,45 @@ def save_client(a,b,c,d,e,f,g,h,i,j):
         "Balance": i,
         "Id": j
     }
-    allClient = client
-    print(allClient)
-### input data ###
+    allCustomers[f"customer{j}"] = customer
+    print(f"customer{j} ")
+    print(allCustomers)
+
+#### main screen ####
 os.system("clear")
 print(".::: Python report generator script  :::.")
-print("Athen enter the client data: ")
+print("Athen enter the customer data: ")
 input("Press enter to start...")
-allClient = {}
+
+#### data entry ####
+allCustomers = {}
 employee = 0
 next = True
 while next:
     os.system("clear")
     fullName = data_valid("Enter the full name: ")
     idNumber = data_valid("Enter the ID number: ")
-    age = int(data_valid("Enter the age: "))
+    age = age_valid("Enter the age: ")
     movilPhone = data_valid("Enter the mobile phone: ")
     address = data_valid("Enter the address: ")
-    clientStatus = True
+    customerStatus = True
     income = get_income()
-    expenses = float(data_valid("Enter the expenses: "))
+    expenses = float(numb_valid("Enter the expenses: "))
     balance = income - expenses
     employee += 1
-    save_client(fullName, idNumber, age, movilPhone, address, clientStatus, income, expenses, balance, employee)
-    ans = input("Do you want to add another client? \n"
+    save_client(fullName, idNumber, age, movilPhone, address, customerStatus, income, expenses, balance, employee)
+    ans = input("Do you want to add another customer? \n"
     "Press [y] to add or any other key to exit...").lower()
     if ans != "y":
         next = False
+
+
+#### report generator ####
+os.system("clear") 
+
+print("::: Report generator :::")
+print("Id - Name - Id Number - Age - Movil Phone - Address - Client Status - Income - Expenses - Balance")
+
+for value in allCustomers.values():
+    print(f"{value['Id']} - {value['Name']} - {value['Id Number']} - {value['Age']} - {value['Movil Phone']} - {value['Address']} - {value['Client Status']} - {value['Income']} - {value['Expenses']} - {value['Balance']}\n")
+
