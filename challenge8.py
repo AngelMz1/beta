d21b6b2b4ec7d444bd91701cbb51933eaaa9fa51
Angel Muñoz <munozbenavidesangel@gmail.com>
2025-07-29 17:00:13 -0500

    desarrollo punto 8 python

--- challenge8.py
+++ challenge8.py
@@ -3,14 +3,26 @@
 2. If the new value to be entered is equal to the SUM of the last and 
 second-to-last values entered, the system must request the number again
 '''
+import os
+os.system("clear")
 ### functions ###
-
+def valid_number():
+    while True:
+        num = input("Enter a number o [q] to exit: ").strip()
+        if num.lower() == "q":
+            break
+        try:
+            num = float(num)
+            if len(numbers) >= 2 and num == numbers[-1] + numbers[-2]:
+                print("The sum of the last two numbers is equal to the new number. Please enter a valid number.")
+                raise ValueError
+            else:
+                numbers.append(num)
+                os.system("clear")
+                print("The numbers entered are: ",numbers)
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 ### main ###
 numbers = []
 
-while True:
-    num = int(input("Enter an integer: "))
-    if len(numbers) >= 2 and num == numbers[-1] + numbers[-2]:
-        print("The number entered is equal to the sum of the last two numbers.")
-        break
-    numbers.append(num)
+valid_number()
